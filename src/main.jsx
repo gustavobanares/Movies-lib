c9d1df398d050cbc3d6d6da352e77cafb723f128
*Gustavo <gustavobbacco@gmail.com>
2024-11-11 18:50:48 -0300

    remove: doing setup to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='movie/:id' element={<Movie />}></Route>
+        <Route path='/search' element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
