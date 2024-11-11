a2bb1cff095c7e933453f95d1316bf5d7f24955e
*Gustavo <gustavobbacco@gmail.com>
2024-11-11 19:18:44 -0300

    feat: create navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
 import App from './App.jsx'
@@ -12,12 +12,13 @@ import Search from './pages/Search.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route element={<App />}></Route>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='movie/:id' element={<Movie />}></Route>
-        <Route path='/search' element={<Search />}></Route>
-      </Routes>
-    </BrowserRouter>
+  <Routes>
+    <Route element={<App />}>
+      <Route path="/" element={<Home />} />
+      <Route path="movie/:id" element={<Movie />} />
+      <Route path="search" element={<Search />} />
+    </Route>
+    </Routes>
+  </BrowserRouter>
   </StrictMode>,
 )
