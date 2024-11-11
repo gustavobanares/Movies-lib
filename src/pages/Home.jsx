7033a5ce2066e5b9b6e7761a6152362edcab5008
*Gustavo <gustavobbacco@gmail.com>
2024-11-11 20:35:26 -0300

    styles: moviesgrid css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 
+import './MoviesGrid.css'
+
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
